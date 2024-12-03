30675331007ccba44146b77fd5a7092002d10838
ShariqShaikh33 <shariqshaikh2705@gmail.com>
2024-12-03 14:31:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -33,24 +33,42 @@ const questions = [
 // Display the quiz questions and choices
 function renderQuestions() {
   for (let i = 0; i < questions.length; i++) {
+	const questionsElement = document.getElementById('questions');
+	let userAnswers = [];
     const question = questions[i];
     const questionElement = document.createElement("div");
+	questionElement.setAttribute("class", "questiondivmain");
     const questionText = document.createTextNode(question.question);
     questionElement.appendChild(questionText);
     for (let j = 0; j < question.choices.length; j++) {
+	  const questiondiv = document.createElement("div");
+	questiondiv.setAttribute("class", "questiondiv");
       const choice = question.choices[j];
       const choiceElement = document.createElement("input");
       choiceElement.setAttribute("type", "radio");
+		choiceElement.setAttribute("id", choice);
       choiceElement.setAttribute("name", `question-${i}`);
       choiceElement.setAttribute("value", choice);
       if (userAnswers[i] === choice) {
         choiceElement.setAttribute("checked", true);
       }
-      const choiceText = document.createTextNode(choice);
-      questionElement.appendChild(choiceElement);
-      questionElement.appendChild(choiceText);
+      const choiceText = document.createElement("label");
+		choiceText.innerHTML = choice;
+		choiceText.setAttribute("for", choice);
+      questiondiv.appendChild(choiceElement);
+      questiondiv.appendChild(choiceText);
+	  questionElement.appendChild(questiondiv);
     }
+	
     questionsElement.appendChild(questionElement);
   }
+	
 }
 renderQuestions();
+
+
+
+
+
+
+
